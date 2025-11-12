90c4f1f30dc7b085439ef36797180d339fadf77e
Vemund Skogen <vskogen@hotmail.no>
2025-11-12 15:46:07 +0100

    started work on home navigation button.

---
+++ MatOppskrift/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // You can add logic here if needed like shared state or imports or functions.
+</script>
+
+<header class="site-header">    
+    <nav class="nav">
+        <a data-sveltekit-prefetch href="/" class="home-btn">Hjem</a>
+        <!-- add other global links here, e.g.:
+    <a href="/foods">Foods</a>
+    -->
+    </nav>
+</header>
+
+<main class="content">
+    <slot />
+</main>
